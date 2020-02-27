7e455d3451aaa0a8bff6c2ae477623df13af967f
Damned Angel <danilo@angelo.eng.br>
2020-02-26 22:48:08 -0300

    [REL_00.04.00] Release created.

--- src/Source Projects/MSX Applications/MSX BIN Application/Make/make.bat
+++ src/Source Projects/MSX Applications/MSX BIN Application/Make/make.bat
@@ -1,7 +1,7 @@
 
 echo -----------------------------------------------------------------------------------
 echo MSX SDCC MAKEFILE by Danilo Angelo, 2020
-echo version 00.04.XX - Codename BLUE
+echo version 00.04.00 - Codename BLUE
 
 set MSX_BUILD_TIME=%TIME% 
 set MSX_BUILD_DATE=%DATE% 
