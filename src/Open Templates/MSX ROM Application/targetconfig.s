6bf7a5debddcaf01b8d2f3ea077f0ab2754f805b
Damned Angel <danilo@angelo.eng.br>
2020-05-17 21:13:47 -0300

    [TKT_00012] Templates for release 00.04.00 generated.

--- src/Open Templates/MSX ROM Application/targetconfig.s
+++ src/Open Templates/MSX ROM Application/targetconfig.s
@@ -1,6 +1,6 @@
 ;-------------------------------------------------		
 ; targetconfig.s created automatically by makefile		
-; on 20:29:29.39 , 17-May-20 				
+; on 20:29:29.47 , 17-May-20 				
 ;														
 ; DO NOT BOTHER EDITING THIS.							
 ; ALL CHANGES WILL BE LOST.							
--- src/Open Templates/MSX-DOS Application/applicationsettings.s
+++ src/Open Templates/MSX-DOS Application/applicationsettings.s
@@ -1,10 +1,13 @@
 ;-------------------------------------------------		
-; targetconfig.s created automatically by makefile		
+; applicationsettings.s created automatically			
+; by makefile											
 ; on 20:29:29.39 , 17-May-20 				
 ;														
 ; DO NOT BOTHER EDITING THIS.							
 ; ALL CHANGES WILL BE LOST.							
 ;-------------------------------------------------		
 														
-DEBUG = 1									
-OPENMSX_PROFILING = 1									
+GLOBALS_INITIALIZER = 1								
+CMDLINE_PARAMETERS = 0								
+paramHandlingRoutine .equ 0				
+PARAM_HANDLING_ROUTINE = 0				
